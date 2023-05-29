6df84734361e66e27b9cd6db8b9bc03a0ed2e0ca
Arko Ghosh Dastidar <arkoghoshdastidar392000@gmail.com>
2023-05-29 16:22:22 +0530

    database connected successfully

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
 
+// configure env
 dotenv.config({ path: 'backend/config/config.env' });
+
+// connect database
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on http://localhost:' + process.env.PORT);
