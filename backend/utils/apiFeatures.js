c5511163baf69a2783fa466006dfa6be484c1931
Arko Ghosh Dastidar <arkoghoshdastidar392000@gmail.com>
2023-06-01 18:01:22 +0530

    price filter feature added to apiFeatures.js

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,27 +1,33 @@
 class ApiFeatures {
     // query is the mongoDB query to be executed. ex:- model.find({});
     // queryStr contains all the keywords in the req query;
-    constructor(query, queryStr){
+    constructor(query, queryStr) {
         this.query = query;
         this.queryStr = queryStr;
     }
 
-    search(){
-        const keyword = (this.queryStr.keyword)?{
+    search() {
+        const keyword = (this.queryStr.keyword) ? {
             name: {
                 $regex: this.queryStr.keyword,
                 $options: 'i'
             }
-        }:{};
+        } : {};
 
-        this.query = this.query.find({...keyword});
+        this.query = this.query.find({ ...keyword });
         return this;
     }
 
-    filter(){
-        let newQueryStr = {...this.queryStr};
+    filter() {
+        let newQueryStr = { ...this.queryStr };
+        // removing some properties which are not useful for the filter method
         const removeProps = ["keyword", "page", "limit"];
         removeProps.forEach(key => delete newQueryStr[key]);
+
+        // filtering on the basis of price, rating and category
+        newQueryStr = JSON.stringify(newQueryStr);
+        newQueryStr = newQueryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+        newQueryStr = JSON.parse(newQueryStr);
         this.query = this.query.find(newQueryStr);
         return this;
     }
