7853697762f9b033303376455e2a2243d476df96
Arko Ghosh Dastidar <arkoghoshdastidar392000@gmail.com>
2023-06-13 18:30:09 +0530

    header component made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
+import Header from './components/layout/Header/Header';
+import webFont from 'webfontloader';
+import { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    webFont.load({
+      google: {
+        'families': ['Roboto',]
+      }
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   );
 }
 
