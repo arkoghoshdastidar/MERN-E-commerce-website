2a43b4c4fc8f9e99675a5451efe98e1a84e6c62a
Arko Ghosh Dastidar <arkoghoshdastidar392000@gmail.com>
2023-06-25 14:55:15 +0530

    login signup form created

--- frontend/src/components/LoginSignup/LoginSignup.js
+++ frontend/src/components/LoginSignup/LoginSignup.js
@@ -1,33 +1,49 @@
 import styles from './LoginSignup.module.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
 const LoginSignup = () => {
     const [isLoggingIn, setIsLoggingIn] = useState(true);
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+
+    const formSubmitHandler = () => {
+        // extracting values from the input fields
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const confirmPassword = (isLoggingIn === false) ? confirmPasswordRef.current.value : null;
+
+        // clearing the input fields
+        emailRef.current.value = "";
+        passwordRef.current.value = "";
+        confirmPasswordRef.current.value = "";
+    }
+
     return (
         <div className={styles['login-signup-container']}>
             <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                <Button>Login</Button>
-                <Button>SignUp</Button>
+                <Button onClick={() => setIsLoggingIn(true)}>Login</Button>
+                <Button onClick={() => setIsLoggingIn(false)}>SignUp</Button>
             </ButtonGroup>
             <form>
                 <div>
                     <label>Email : </label>
-                    <input type="email" name="email" placeholder="example@example.com" />
+                    <input ref={emailRef} type="email" name="email" placeholder="example@example.com" />
                 </div>
                 <div>
                     <label>Password : </label>
-                    <input type="text" name="password" placeholder='password' />
+                    <input ref={passwordRef} type="password" name="password" placeholder='password' />
                 </div>
-                <div>
+                {!isLoggingIn && <div>
                     <label>Confirm Password : </label>
-                    <input type="text" name="confirm_password" placeholder='confirm password' />
-                </div>
+                    <input ref={confirmPasswordRef} type="password" name="confirm_password" placeholder='confirm password' />
+                </div>}
             </form>
             <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                <Button>Submit</Button>
+                <Button onClick={formSubmitHandler}>Submit</Button>
             </ButtonGroup>
         </div>
     )
